a9e7ca6d4acf06e54b46a62613d80004295cbb52
Dindu5 <chimdindue@gmail.com>
2020-07-30 22:02:26 +0100

    added top nav and corrections

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,6 +11,7 @@
 
 //new Chartist.Line('.ct-chart2', data);
 
+// bar chart
 new Chartist.Bar(
 	'.ct-chart2',
 	{
@@ -27,6 +28,7 @@ new Chartist.Bar(
 	}
 );
 
+// pie chart
 var chart = new Chartist.Pie(
 	'.ct-chart',
 	{
@@ -38,17 +40,14 @@ var chart = new Chartist.Pie(
 	}
 );
 
+// pie chart animation
 chart.on('draw', function(data) {
 	if (data.type === 'slice') {
-		// Get the total path length in order to use for dash array animation
 		var pathLength = data.element._node.getTotalLength();
-
-		// Set a dasharray that matches the path length as prerequisite to animate dashoffset
 		data.element.attr({
 			'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
 		});
 
-		// Create animation definition while also assigning an ID to the animation for later sync usage
 		var animationDefinition = {
 			'stroke-dashoffset': {
 				id: 'anim' + data.index,
@@ -56,28 +55,23 @@ chart.on('draw', function(data) {
 				from: -pathLength + 'px',
 				to: '0px',
 				easing: Chartist.Svg.Easing.easeOutQuint,
-				// We need to use `fill: 'freeze'` otherwise our animation will fall back to initial (not visible)
+
 				fill: 'freeze'
 			}
 		};
 
-		// If this was not the first slice, we need to time the animation so that it uses the end sync event of the previous animation
 		if (data.index !== 0) {
 			animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
 		}
 
-		// We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
 		data.element.attr({
 			'stroke-dashoffset': -pathLength + 'px'
 		});
 
-		// We can't use guided mode as the animations need to rely on setting begin manually
-		// See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
 		data.element.animate(animationDefinition, false);
 	}
 });
 
-// For the sake of the example we update the chart every time it's created with a delay of 8 seconds
 chart.on('created', function() {
 	if (window.__anim21278907124) {
 		clearTimeout(window.__anim21278907124);
@@ -86,6 +80,7 @@ chart.on('created', function() {
 	window.__anim21278907124 = setTimeout(chart.update.bind(chart), 10000);
 });
 
+// Addition of carousel on small screens
 $(document).ready(function() {
 	if ($(window).width() < 767) {
 		var owl = $('#slide').addClass('owl-carousel');
